9a83f5dc68f0686efd33ba15e828d3fa05bd3d9b
Jonathan Schear <jonathanschear@gmail.com>
2023-01-20 22:26:34 -0500

    Typescript

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+export const authOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_ID,
+      clientSecret: process.env.SPOTIFY_SECRET,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
